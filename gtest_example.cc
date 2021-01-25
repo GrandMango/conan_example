d7528ebe4d4297a8706fd5c007976f46953a4163
yuecheng.pei <793499453@qq.com>
2021-01-25 09:59:37 +0800

    Add interface

--- gtest_example.cc
+++ gtest_example.cc
@@ -1,10 +1,19 @@
 #include <gtest/gtest.h>
+#include <iostream>
+
+namespace conan {
+	void interface() {
+		std::cout << "Calling Interface" << std::endl;
+	}	
+}
 
 TEST(TEST_EXAMPLE, should_return_true) {
 	EXPECT_EQ(1, 1);
 }
 
 int main(int argc, char *argv[]) {
+	conan::interface();
+
     testing::InitGoogleTest(&argc,argv);
 	return RUN_ALL_TESTS();
 }
